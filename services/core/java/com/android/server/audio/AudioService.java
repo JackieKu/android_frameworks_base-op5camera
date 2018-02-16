bb98490b2606f0c34f5a5a8c62b52d9229d79513
Kai-Chieh Ku <kjackie@gmail.com>
2018-02-16 12:55:36 +0800

    Allow forced camera sound being disabled by system property
    Change-Id: I0cd7ed88af892ec531713ce502df0a190581e00d

--- services/core/java/com/android/server/audio/AudioService.java
+++ services/core/java/com/android/server/audio/AudioService.java
@@ -6190,6 +6190,7 @@ public class AudioService extends IAudioService.Stub
     //==========================================================================================
     private boolean readCameraSoundForced() {
         return SystemProperties.getBoolean("audio.camerasound.force", false) ||
+                SystemProperties.getBoolean("persist.sys.camera-sound", true) &&
                 mContext.getResources().getBoolean(
                         com.android.internal.R.bool.config_camera_sound_forced);
     }
